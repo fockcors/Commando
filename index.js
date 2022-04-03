258fbea3e9fcc4b504b701fcc31b8e4bd3514c6f
fockcors <fockcors@users.noreply.github.com>
2022-04-03 02:55:24 +0200

    Add PORT env var

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (fs.existsSync(`${__dirname}/data.json`)) {
     appData = require(`${__dirname}/data.json`);
 }
 
-const server = app.listen(3987);
+const server = app.listen(process.env.PORT || 3987);
 const wsServer = new ws.Server({ server: server, path: '/api/ws' });
 
 app.use('/maps', (req, res, next) => {
